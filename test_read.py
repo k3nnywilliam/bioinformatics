4b4b2cb1b9a0046067c44e0c1bbe5e9cce3e6f32
Kenny William Nyallau <k3nnywilliam@gmail.com>
2020-06-18 21:02:11 +0800

    Minor update to test_read

--- test_read.py
+++ test_read.py
@@ -1,10 +1,10 @@
 
-def test_read():
-   with open("data/dataset_2_7.txt", "r") as f:
+def test_read(file):
+   with open(file, "r") as f:
        lines = f.read().splitlines()
        genome = lines[0]
        pattern = lines[-1]#the pattern is on the last line of text file
        print(f"Genome: {genome}")
        print(f"K-mer pattern: {pattern}")
        
-test_read()
+test_read("data/dataset_2_7.txt")
